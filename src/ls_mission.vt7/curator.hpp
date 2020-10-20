e45d92ae0ff119ea3227f0e3a585333cd687c18b
SCHTAMP <ylyka0@ya.ru>
2020-10-20 14:01:20 +0300

    Test

---
+++ src/ls_mission.vt7/curator.hpp
@@ -0,0 +1,31 @@
+class Curators
+{
+	list[] = 
+	{
+        "76561198057238748"//Host (Creator)
+        ,"76561198079947268"//Shepard (Curator)
+		,"76561198121927537"//Morket (SudoCurator)
+		,"76561198072246605"//SCHTAMP (Tech)
+		,"76561198288271495"//KapayJl (Tech)
+		,"76561198166050664"//Foxxy (EventCurator)
+		,"76561199028516754"//Desire (Tech)
+		,"76561198182854197"//YokaEnenra (Event)     
+        ,"76561198209489483"//PO(Event)
+        ,"76561198253231135"//BKA (Event)
+        ,"76561198106601977"//Ximik (Event)
+        ,"76561199071806700"//Leogorn(Event)
+		,"76561198144015474"//Kelt(Event)
+        ,"76561198247446122"//AveMatria(Event)
+		,"76561198817496048"//Jind(Event)
+        ,"76561198033904243"//Jisai (Donate_Zeus)
+		,"76561198195478551"//Mergen (NeutralCurator)
+		,"76561198227583576"//Filin (EventNeutral)
+		,"76561198403064350"//Smetanka (EventNeutral)
+		,"76561198114414576"//Auberon fon Thorne(JediCurator)
+        ,"76561198084192447"//Irai(EventJedi)
+		,"76561198102983006"//Joker (Gray)
+        ,"76561198351130485"//Bybylyka (Gray)
+		,"76561198006688545"//Dark (BSO)
+        ,"76561198053534432"//Redcrown(Rp)        	
+	};
+};
